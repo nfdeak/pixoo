9cd2af4eaadc90dbe0c3cae601aacb0bbb681db5
Deak Norbert <deaknorbert@Deaks-MacBook-Air.local>
2020-04-08 20:42:44 +0300

    frames added to header

--- pixoo-app/src/components/Header.js
+++ pixoo-app/src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Flex , Box, Text  } from '@chakra-ui/core';
+import { Flex , Box, Text, Image  } from '@chakra-ui/core';
 import { TiThMenu ,TiChevronLeft } from "react-icons/ti";
+import white from '../resources/frame1.svg';
+import black from '../resources/frame2.svg';
+import mocha from '../resources/frame3.svg';
+import latte from '../resources/frame4.svg';
 
 function Header ({}) {
 
@@ -19,10 +23,26 @@ function Header ({}) {
                 alignItems="center"
                 justifyContent="center"
             >
-                <Box w="85px" h="100px" bg="tomato"></Box>
-                <Box w="85px" h="100px" bg="blue"></Box>
-                <Box w="85px" h="100px" bg="red"></Box>
-                <Box w="85px" h="100px" bg="tomato"></Box>
+                <Flex backgroundImage="linear-gradient(0deg,rgba(255,0,149,.25),rgba(255,0,149,0))" w="85px" h="100px" alignItems="center"
+                      justifyContent="center" direction="column">
+                    <Image src={white} w="65px" />
+                    <Text>White</Text>
+                </Flex>
+                <Flex w="85px" h="100px" alignItems="center"
+                      justifyContent="center" direction="column">
+                    <Image src={black} w="65px" />
+                    <Text>Black</Text>
+                </Flex>
+                <Flex w="85px" h="100px" alignItems="center"
+                      justifyContent="center" direction="column">
+                    <Image src={mocha} w="65px" />
+                    <Text>Mocha</Text>
+                </Flex>
+                <Flex w="85px" h="100px" alignItems="center"
+                      justifyContent="center" direction="column">
+                    <Image src={latte} w="65px" />
+                    <Text>Latte</Text>
+                </Flex>
             </Flex>
         </Box>
     );
