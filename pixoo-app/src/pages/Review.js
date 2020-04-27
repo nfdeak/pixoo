9d3ce867622d8bf9bca6e8c724b181b83e4408bc
deaknorbert <deak.norbert@outlook.com>
2020-04-27 22:55:19 +0300

    corrected bug wwith useeeffect

--- pixoo-app/src/pages/Review.js
+++ pixoo-app/src/pages/Review.js
@@ -24,15 +24,11 @@ function Review() {
 
     const addNewPhoto  = (base64URL, position) => {
         setPhotosArray(position === 'right' ?  photosArray => photosArray.concat([{src:base64URL, id: Date.now()}]): photosArray => [{src:base64URL, id: Date.now()}].concat(photosArray));
-        window.innerWidth> 760 ? document.getElementById('right').scrollIntoView({ inline: 'center',behavior: 'smooth' }) : position === 'right' ? document.getElementById('last').scrollIntoView({ inline: 'center',behavior: 'smooth' }) : document.getElementById('first').scrollIntoView({ inline: 'center',behavior: 'smooth' });
+        window.innerWidth> 760 ? document.getElementById('right').scrollIntoView({ inline: 'center',behavior: 'smooth' }) : position === 'right' ? (document.getElementById('last').scrollIntoView({ inline: 'center',behavior: 'smooth' }) ): document.getElementById('first').scrollIntoView({ inline: 'center',behavior: 'smooth' });
     }
 
-    useEffect(() => {
-        photosArray.length==1 ? document.getElementById('first').scrollIntoView({ inline: 'center',behavior: 'smooth' }) : undefined;
-    }, [photosArray]);
-
     const photosInFrames = photosArray.map((photo,index) =>
-        <Grid id={index==0 ? 'first': index+1 == photosArray.length ? 'last':undefined} h="253px" minW="253px" mx={2} key={photo.id} css={css`animation: ${keyframes.fadeIn} 0.5s ease;`}>
+        <Grid id={index === 0 ? 'first': index+1 === photosArray.length ? 'last':undefined} h="253px" minW="253px" mx={2} key={photo.id} css={css`animation: ${keyframes.fadeIn} 0.5s ease;`}>
             <Image maxW="100%" maxH="100%" minW="100%" minH="100%" p={2} src={photo.src} objectFit="cover" gridArea="1 / 1"/>
             <Image maxW="100%" maxH="100%" minW="100%" minH="100%" src={selectedFrame} gridArea="1 / 1"/>
         </Grid>);
@@ -44,15 +40,15 @@ function Review() {
                   w="100%"
                   h="100%"
                   alignItems="center"
-                  justifyContent={photosArray.length==0? "center": "flex-start"}
+                  justifyContent={photosArray.length===0? "center": "flex-start"}
                   overflowX="auto"
                   px={2}
                   css={css`-ms-overflow-style: none;scrollbar-width: none;::-webkit-scrollbar{display: none;}`}
             >
-                {photosArray.length>0? <Flex mx={2} display={["flex","flex","none","none"]}><UploadSquare isAnimating={ false} onUploadPhoto={addNewPhoto} position="left"/></Flex>: undefined}
+                <Flex mx={2}><UploadSquare isAnimating={photosArray.length===0} onUploadPhoto={addNewPhoto} position="left"/></Flex>
                 {photosInFrames}
-                <Flex mx={2}><UploadSquare isAnimating={ photosArray.length==0 ? true : false} onUploadPhoto={addNewPhoto} position="right"/></Flex>
-                {photosArray.length>0? <Flex minW={2} visibility="hidden">.</Flex>:undefined}
+                {photosArray.length>0 ? <Flex mx={2} display={["flex","flex","none","none"]}><UploadSquare isAnimating={ false} onUploadPhoto={addNewPhoto} position="right"/></Flex>:undefined}
+                {photosArray.length>0 ? <Flex minW={2} visibility="hidden">.</Flex>:undefined}
             </Flex>
             <Drawer isOpen={isOpen} placement="bottom" onClose={onClose}>
                 <DrawerOverlay/>
