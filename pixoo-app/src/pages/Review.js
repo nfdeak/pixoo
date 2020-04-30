4cb113a133dcc1e0a1e352eb6985ca67294ea3e1
deaknorbert <deak.norbert@outlook.com>
2020-04-30 22:24:03 +0300

    changed layout-working

--- pixoo-app/src/pages/Review.js
+++ pixoo-app/src/pages/Review.js
@@ -28,11 +28,11 @@ function Review() {
 
     const addNewPhoto  = (base64URL, position) => {
         setPhotosArray(position === 'right' ?  photosArray => photosArray.concat([{src:base64URL, id: Date.now()}]): photosArray => [{src:base64URL, id: Date.now()}].concat(photosArray));
-        window.innerWidth> 760 ? document.getElementById('right').scrollIntoView({ inline: 'center',behavior: 'smooth' }) : position === 'right' ? (document.getElementById('last').scrollIntoView({ inline: 'center',behavior: 'smooth' }) ): document.getElementById('first').scrollIntoView({ inline: 'center',behavior: 'smooth' });
+        window.innerWidth> 760 ? document.getElementById('right').scrollIntoView({ inline: 'center',behavior: 'smooth' }) : position === 'right' ? document.getElementById('last').scrollIntoView({ inline: 'center',behavior: 'smooth' }) : document.getElementById('first').scrollIntoView({ inline: 'center',behavior: 'smooth' });
     }
 
     const photosInFrames = photosArray.map((photo,index) =>
-        <Grid onClick={onOpenModal} id={index === 0 ? 'first': index+1 === photosArray.length ? 'last':undefined} h="253px" minW="253px" maxW="253px" maxH="253px" key={photo.id} css={css`animation: ${keyframes.fadeIn} 0.5s ease;`} cursor="pointer">
+        <Grid onClick={onOpenModal} id={photosArray.length===1 ? 'last' : index === 0 ? 'first': index+1 === photosArray.length ? 'last' : undefined} h="253px" minW="253px" maxW="253px" maxH="253px" key={photo.id} css={css`animation: ${keyframes.fadeIn} 0.5s ease;`} cursor="pointer">
             <Image maxW="100%" maxH="100%" minW="100%" minH="100%" p={2} src={photo.src} objectFit="cover" gridArea="1 / 1"/>
             <Image maxW="100%" maxH="100%" minW="100%" minH="100%" src={selectedFrame} gridArea="1 / 1"/>
         </Grid>);
@@ -43,13 +43,12 @@ function Review() {
             <Flex bgImage={"url(" + bg + ")"} bgPos="center top" bgRepeat="no-repeat" bgSize="cover"
                   w="100%"
                   h="100%"
-                  alignContent={"center"}
                   alignItems="center"
-                  justifyContent={"center"}
+                  justifyContent={[photosArray.length===0 ? "center" : "flex-start",photosArray.length===0 ? "center" : "flex-start","center"]}
                   overflow="auto"
-                  px={"20%"}
+                  px={[2,2,"20%"]}
                   css={css`-ms-overflow-style: none;scrollbar-width: none;::-webkit-scrollbar{display: none;}`}
-                  flexWrap={window.innerWidth> 760 ? "wrap" : "no-wrap"}
+                  flexWrap={["no-wrap","no-wrap","wrap"]}
             >
                 {photosArray.length > 0 && <Flex display={["flex","flex","none","none"]}><UploadSquare isAnimating={false} onUploadPhoto={addNewPhoto} position="left"/></Flex>}
                 {photosInFrames}
